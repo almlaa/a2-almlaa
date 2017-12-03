f98fa3348abe4356f68377b1f839f9bda55e6894
almlaa <almla671@gmail.com>
2017-12-03 13:33:30 +0200

    added some changes

--- a2_process_data.py
+++ a2_process_data.py
@@ -21,54 +21,72 @@ with open("a2_input.csv") as input_file:
 
 
 
-###print("All it can do is print out the contents of a couple of cells of the file a2_input.csv:")
-###print("Cells at index 5")
-###print(contents[5])
-###print(type(contents[5]))
-###print("Cell at index 13,6:")
-###print(contents[13][6])
-###print(type(contents[13][6]))
-###print("Cell at index 10,2:")
-###print(contents[10][2])
-###print(type(contents[10][2]))
-###print("Cell at index 1,0:")
-###print(contents[1][0])
-###print(type(contents[1][0]))
-###a = int(contents[3][1])
-###b = int(contents[3][2])
-###c = int(contents[3][3])
-###total_of_germany = a + b + c
-###print("Total german tourists " + str(total_of_germany))
+###				print("All it can do is print out the contents of a couple of cells of the file a2_input.csv:")
+#				print("Cells at index 5")
+##				print(contents[5])
+########		print(type(contents[5]))
+#######			print("Cell at index 13,6:")
+######			print(contents[13][6])
+#####			print(type(contents[13][6]))
+####			print("Cell at index 10,2:")
+###				print(contents[10][2])
+##				print(type(contents[10][2]))
+########		print("Cell at index 1,0:")
+#######			print(contents[1][0])
+######			print(type(contents[1][0]))
+###				a = int(contents[3][1])
+######			b = int(contents[3][2])
+###				c = int(contents[3][3])
+#####			total_of_germany = a + b + c
+#######			print("Total german tourists " + str(total_of_germany))
 
 
-###print("Nations:coming tourists in 2014,2015,2016")
-###print("cell at index 3:7,0:3")
-###i=3
-###while(i<=7):
-######	print(contents[i][0] + ":" + " " + contents[i][1]  + " " + "," + " " + contents[i][2] + " "  +"," +  " " + contents[i][3])
-######	i=i+1
-###print(type(contents[3:7][0:3]))
+###    			print("Nations:coming tourists in 2014,2015,2016")
+###				print("cell at index 3:7,0:3")
+####			i=3
+#####			while(i<=7):
+######		    	print(contents[i][0] + ":" + " " + contents[i][1]  + " " + "," + " " + contents[i][2] + " "  +"," +  " " + contents[i][3])
+#######		    	i=i+1
+########		print(type(contents[3:7][0:3]))
+	
+###			x=input("which order do you want in nations")
+###			if x in range(1,20):
+######			print(contents[int(x)+2][0])
+######				else:
+######					print('There are only 20 nations')
+###			print(type(contents[int(x)+2][0]))
+###			print(contents[dog][cat]) = (NameError: name 'dog' is not defined)
+####		contents[5][3] + int(contents[3][3]) = TypeError: cannot concatenate 'str' and 'int' objects
+#####		print(contents[5][3]*int(contents[3][3]))= became a loop
+####		help(contents[5][3])=no Python documentation found for '906336'
 
-###x=input("which order do you want in nations")
-
-###if x in range(1,20):
-######	print(contents[int(x)+2][0])
-###else:
-######	print('There are only 20 nations')
-###print(type(contents[int(x)+2][0]))
 
 print("<!DOCTYPE html>")
 print("<html lang=\"en\">")
 print("<head>")
 print("<title>Coming tourists to the Nations</title>")
 print("<meta charset=\"utf-8\" />")
+print(""" <style>
+  table{ backgraund-color:#f6fca4
+  }
+
+  td{color:#306812
+  border: 1.5px solid
+  } 
+  
+  th{color:#68112a
+   border: 1.5px solid
+  }
+
+
+<style/> """)
 print("</head>")
 print("<body>")
 
-print("<table border= solid 1px >")
+print("<table>")
 a=0
 
-while (a<=24):
+while (a<=23):
 	b=0
 	print('<tr>')
 	while (b<=6):
@@ -77,7 +95,7 @@ while (a<=24):
 		else:
 			print('<td>')
 		print(contents[a][b]) 
-		if a in (1,2):
+		if a in (0,1,2):
 			print('</th>')
 		else:
 			print('</td>')
@@ -87,6 +105,8 @@ while (a<=24):
 	a += 1
 
 print("</table>")
+
+print("<p>Total of nations who increased 2014 to 2015:6337656 </p>")
 print("</body>")
 print("</html>")
 	
